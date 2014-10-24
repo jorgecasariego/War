4fc1e3a96a726de6b08e84f277506d353c73fbf6
Jorge Casariego <jorgekasa@gmail.com>
2014-10-24 20:23:35 -0300

    Finished the War App

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -15,13 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var playRoundButton: UIButton!
     @IBOutlet weak var backgroundImageView: UIImageView!
     
+    
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var enemyScore: UILabel!
+    
+    
     var arrayCartas:[String] = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13",]
+    
+    var playerScoreTotal = 0
+    var enemyScoreTotal = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
+        //self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,6 +46,20 @@ class ViewController: UIViewController {
         //Seteando cartas aleatoriamente
         self.firstCardImageView.image = UIImage(named: arrayCartas[carta1])
         self.seconfCardImageView.image = UIImage(named: arrayCartas[carta2])
+        
+        // Determine the higher card
+        if carta1 > carta2 {
+            // First card is larger
+            playerScoreTotal += 1
+            self.playerScore.text = String(playerScoreTotal)
+            
+        } else if carta1 == carta2{
+            
+        } else{
+            //  Second card is larger
+            enemyScoreTotal += 1
+            self.enemyScore.text = String(enemyScoreTotal)
+        }
         
         
     }
