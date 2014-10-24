8b270e0ab2c5a5fa5098840c88b10205e2dff81b
Jorge Casariego <jorgekasa@gmail.com>
2014-10-24 17:00:36 -0300

    War Game in Swift
    Is a game with 2 cards.
    In this example I create a layout with 2 cards and a button. Every time
    I press the button cards change in a random way.

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var seconfCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    var arrayCartas:[String] = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playRoundTapped(sender: UIButton) {
+        
+        //Obteniendo numeros aleatorios
+        var carta1:Int = Int(arc4random_uniform(13))
+        var carta2:Int = Int(arc4random_uniform(13))
+        
+        //Seteando cartas aleatoriamente
+        self.firstCardImageView.image = UIImage(named: arrayCartas[carta1])
+        self.seconfCardImageView.image = UIImage(named: arrayCartas[carta2])
+        
+        
+    }
 
 }
 
